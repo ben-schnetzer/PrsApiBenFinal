aaa8dacb8d33818c06bda97d1afa0c7944f4bed3
Ben Schnetzer <schnetzer.ben@gmail.com>
2025-05-08 16:49:38 -0400

    Initial Commit
    Had to restart for a third time due to technical difficulties.

---
+++ PrsApi/PrsApi/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using PrsApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<PrsDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("PrsApi")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
